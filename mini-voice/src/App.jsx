c45697575c67be7bb752fa7cd89796806f92252e
officialij2005@gmail.com <officialij2005@gmail.com>
2026-01-13 15:35:56 +0800

    clean start

--- mini-voice/src/App.jsx
+++ mini-voice/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>A Simple Invoice System that:</h1>
+        <p>Takes item name(s)</p>
+        <p> Takes quantity </p> 
+        <p>Takes price per item</p> 
+        <p>Calculates totals</p>
+        <p>Displays a clear, formatted invoice</p>  
+
+        <p>Additionally, you must include at least one custom feature.</p>
+
       </div>
-      <h1>hello world</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
